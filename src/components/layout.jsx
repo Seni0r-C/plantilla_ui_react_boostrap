49c1babeface607d919b39958b342a5f85b47793
Carlos Arteaga <carlosluis199924@hotmail.com>
2024-09-24 22:38:33 -0500

    no deja entrar sin autenticarte

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,19 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import NavigationBar from './Navbar';
 
 const Layout = ({ children }) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem('token'); // Asegúrate de que el nombre del token sea correcto
+        if (!token) {
+            // Si no hay token, redirige al usuario al login
+            navigate('/login');
+        }
+    }, [navigate]);
+
     return (
         <>
             <NavigationBar />
